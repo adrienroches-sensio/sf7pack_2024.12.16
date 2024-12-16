9694848433dbd18a84dd229a0b2a68f43a52da05
Adrien Roches <adrien@roc-it.tech>
2024-12-16 10:09:34 +0100

    Use event repository in database event search class

--- src/Search/DatabaseEventSearch.php
+++ src/Search/DatabaseEventSearch.php
@@ -4,10 +4,21 @@ declare(strict_types=1);
 
 namespace App\Search;
 
+use App\Repository\EventRepository;
+
 final class DatabaseEventSearch
 {
+    public function __construct(
+        private readonly EventRepository $eventRepository,
+    ) {
+    }
+
     public function searchByName(string|null $name = null): array
     {
-        return [];
+        if (null === $name) {
+            return $this->eventRepository->findAll();
+        }
+
+        return $this->eventRepository->searchByName($name);
     }
 }
