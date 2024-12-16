8e8f5c499c42d99d044506df81d592535873ef45
Adrien Roches <adrien@roc-it.tech>
2024-12-16 11:27:18 +0100

    Declare and inject devevents api key into ApiEventSearch class

--- src/Search/ApiEventSearch.php
+++ src/Search/ApiEventSearch.php
@@ -4,8 +4,18 @@ declare(strict_types=1);
 
 namespace App\Search;
 
+use SensitiveParameter;
+use Symfony\Component\DependencyInjection\Attribute\Autowire;
+
 final class ApiEventSearch implements EventSearchInterface
 {
+    public function __construct(
+        #[Autowire(env: 'DEVEVENTSAPI_KEY')]
+        #[SensitiveParameter]
+        private readonly string $apiKey,
+    ) {
+    }
+
     public function searchByName(?string $name = null): array
     {
         return [];
