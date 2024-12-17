760f9f6728c03fda38e34cd5b373066aea2b0ebb
Adrien Roches <adrien@roc-it.tech>
2024-12-17 14:39:48 +0100

    Use serailizer for volunteers API

--- src/Controller/Api/VolunteerController.php
+++ src/Controller/Api/VolunteerController.php
@@ -5,13 +5,20 @@ declare(strict_types=1);
 namespace App\Controller\Api;
 
 use App\Repository\VolunteerRepository;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpKernel\Attribute\AsController;
 use Symfony\Component\Routing\Attribute\Route;
+use Symfony\Component\Serializer\SerializerInterface;
 
+#[AsController]
 final class VolunteerController
 {
     #[Route(path: '/api/volunteers', name: 'api_volunteer_list', methods: ['GET'])]
-    public function getVolunteers(VolunteerRepository $volunteerRepository): array
+    public function getVolunteers(VolunteerRepository $volunteerRepository, SerializerInterface $serializer): JsonResponse
     {
-        return $volunteerRepository->findAll();
+        return new JsonResponse(
+            $serializer->serialize($volunteerRepository->findAll(), 'json'),
+            json: true
+        );
     }
 }
