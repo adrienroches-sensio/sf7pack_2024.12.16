0b86a09642eceff3db80ec3bf8e40ae3ee10b15a
Adrien Roches <adrien@roc-it.tech>
2024-12-17 13:13:31 +0100

    Add cacheable voter

--- src/Security/EventVoter.php
+++ src/Security/EventVoter.php
@@ -6,9 +6,11 @@ namespace App\Security;
 
 use Symfony\Component\Security\Core\Authentication\Token\TokenInterface;
 use Symfony\Component\Security\Core\Authorization\AuthorizationCheckerInterface;
+use Symfony\Component\Security\Core\Authorization\Voter\CacheableVoterInterface;
 use Symfony\Component\Security\Core\Authorization\Voter\VoterInterface;
+use function str_starts_with;
 
-final class EventVoter implements VoterInterface
+final class EventVoter implements VoterInterface, CacheableVoterInterface
 {
     public function __construct(
         private readonly AuthorizationCheckerInterface $authorizationChecker,
@@ -32,5 +34,15 @@ final class EventVoter implements VoterInterface
         }
 
         return self::ACCESS_DENIED;
+    }
+
+    public function supportsAttribute(string $attribute): bool
+    {
+        return str_starts_with($attribute, 'event/');
+    }
+
+    public function supportsType(string $subjectType): bool
+    {
+        return true;
     }
 }
